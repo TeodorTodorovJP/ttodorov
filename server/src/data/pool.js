f049aa9271ae39c6f02f71fbabadc843eb6bc4b1
Teodor Todorov <ttodorov.jp@gmail.com>
2022-04-22 09:11:22 +0300

    Fixed jaws property name

--- server/src/data/pool.js
+++ server/src/data/pool.js
@@ -1,6 +1,6 @@
 import { DB_CONFIG } from '../config.js';
 import mariadb from 'mariadb';
 
-const pool = mariadb.createPool(process.env.JAWSDB_MARIADB_URL || DB_CONFIG);
+const pool = mariadb.createPool(process.env.JAWSDB_MARIA_URL || DB_CONFIG);
 
 export default pool;
