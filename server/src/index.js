ade9ae9b4ea4f9d7bcba234dcb9d26fb4b846ef9
Teodor Todorov <ttodorov.jp@gmail.com>
2022-04-22 13:22:53 +0300

    Added logs to test env

--- server/src/index.js
+++ server/src/index.js
@@ -22,9 +22,10 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 // D:\my-websites\ttodorov\server\src
-
+console.log("Env *1* " + process.env.NODE_ENV);
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
+  console.log("Env *2* " + process.env.NODE_ENV);
   console.error(`Node cluster master ${process.pid} is running`);
 
   // Fork workers.
@@ -32,34 +33,42 @@ if (!isDev && cluster.isMaster) {
     cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
   });
-
 } else {
+  console.log("Env *3* " + process.env.NODE_ENV);
   const app = express();
   app.use(cors());
   app.use(helmet());
   app.use(express.json());
 
   // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../../client/build')));
+  app.use(express.static(path.resolve(__dirname, "../../client/build")));
 
-  app.use('/users', usersController);
-  app.use('/admins', adminsController);
+  app.use("/users", usersController);
+  app.use("/admins", adminsController);
 
-  app.use('/app', express.static('public/avatars'));
+  app.use("/app", express.static("public/avatars"));
 
   passport.use(jwtStrategy);
   app.use(passport.initialize());
-  app.use('/auth', authController);
+  app.use("/auth", authController);
 
   // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
+  app.get("*", function (request, response) {
+    response.sendFile(
+      path.resolve(__dirname, "../../client/build", "index.html")
+    );
   });
 
   app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+    console.error(
+      `Node ${
+        isDev ? "dev server" : "cluster worker " + process.pid
+      }: listening on port ${PORT}`
+    );
   });
 }
