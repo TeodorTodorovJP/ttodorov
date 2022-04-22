f02d171e79640b0b6dfb447dda5265e87b291e01
Teodor Todorov <ttodorov.jp@gmail.com>
2022-04-22 11:03:12 +0300

    Changed pool

--- server/src/data/pool.js
+++ server/src/data/pool.js
@@ -1,10 +1,16 @@
 import { DB_CONFIG } from '../config.js';
-import mariadb from 'mariadb';
-console.log("## JAWSDB_MARIADB_URL ## " + process.env.JAWSDB_MARIA_URL);
+import mariadb from "mariadb";
 
-console.log("## process env ## " + JSON.stringify(process.env));
+let createPoolWith = {};
+if (process.env.JAWSDB_MARIA_URL) {
+  createPoolWith = {
+    host: "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "oshn0n1uh3v3ai0t",
+  };
+} else {
+  createPoolWith = DB_CONFIG;
+}
 
-
-const pool = mariadb.createPool(process.env.JAWSDB_MARIA_URL || DB_CONFIG);
+const pool = mariadb.createPool(createPoolWith);
 
 export default pool;
