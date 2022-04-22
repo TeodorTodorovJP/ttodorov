c8f9b690f6bd30ed2d8bda724a2732fb4d1ba16f
Teodor Todorov <ttodorov.jp@gmail.com>
2022-04-22 10:38:47 +0300

    Added logs to debug process.env

--- server/src/data/pool.js
+++ server/src/data/pool.js
@@ -1,6 +1,10 @@
 import { DB_CONFIG } from '../config.js';
 import mariadb from 'mariadb';
-console.log("## JAWSDB_MARIADB_URL ## " + process.env.JAWSDB_MARIADB_URL)
-const pool = mariadb.createPool(process.env.JAWSDB_MARIADB_URL || DB_CONFIG);
+console.log("## JAWSDB_MARIADB_URL ## " + process.env.JAWSDB_MARIA_URL);
+
+console.log("## process env ## " + JSON.stringify(process.env));
+
+
+const pool = mariadb.createPool(process.env.JAWSDB_MARIA_URL || DB_CONFIG);
 
 export default pool;
