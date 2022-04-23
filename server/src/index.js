6ff540c7b9797b023471e301142dd9412f15eb66
Teodor Todorov <ttodorov.jp@gmail.com>
2022-04-23 08:34:24 +0300

    Forks brake the app right away

--- server/src/index.js
+++ server/src/index.js
@@ -16,53 +16,56 @@ const isDev = process.env.NODE_ENV !== "production";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, "../../client/build")));
+
+app.use("/users", usersController);
+app.use("/admins", adminsController);
+
+app.use("/app", express.static("public/avatars"));
+
+passport.use(jwtStrategy);
+app.use(passport.initialize());
+app.use("/auth", authController);
+
+// All remaining requests return the React app, so it can handle routing.
+app.get("*", function (request, response) {
+  response.sendFile(
+    path.resolve(__dirname, "../../client/build", "index.html")
+  );
+});
+
+app.listen(PORT, function () {
+  console.error(
+    `Node ${
+      isDev ? "dev server" : "cluster worker " + process.pid
+    }: listening on port ${PORT}`
+  );
+});
+
+
 // Multi-process to utilize all CPU cores.
-import cluster from "cluster";
-import os from "os";
-const numCPUs = os.cpus().length;
-if (!isDev && cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+// import cluster from 'cluster';
+// import os from 'os';
+// const numCPUs = os.cpus().length;
+// if (!isDev && cluster.isMaster) {
+//   console.error(`Node cluster master ${process.pid} is running`);
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+//   // Fork workers.
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
 
-  cluster.on("exit", (worker, code, signal) => {
-    console.error(
-      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
-    );
-  });
-} else {
-  const app = express();
-  app.use(cors());
-  app.use(helmet());
-  app.use(express.json());
-
-  // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, "../../client/build")));
-
-  app.use("/users", usersController);
-  app.use("/admins", adminsController);
-
-  app.use("/app", express.static("public/avatars"));
-
-  passport.use(jwtStrategy);
-  app.use(passport.initialize());
-  app.use("/auth", authController);
-
-  // All remaining requests return the React app, so it can handle routing.
-  app.get("*", function (request, response) {
-    response.sendFile(
-      path.resolve(__dirname, "../../client/build", "index.html")
-    );
-  });
-
-  app.listen(PORT, function () {
-    console.error(
-      `Node ${
-        isDev ? "dev server" : "cluster worker " + process.pid
-      }: listening on port ${PORT}`
-    );
-  });
-}
+//   cluster.on("exit", (worker, code, signal) => {
+//     console.error(
+//       `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+//     );
+//   });
+// } else {
+//   // express logic
+// }
