81ab134abf56a00395ca1ceebdb26ffdd0cb71ee
Teodor Todorov <ttodorov.jp@gmail.com>
2022-04-23 08:30:50 +0300

    Returned the fork logic to attempt to fix the connections issue

--- server/src/data/pool.js
+++ server/src/data/pool.js
@@ -3,19 +3,6 @@ import mariadb from "mariadb";
 
 // Connection String mysql://oshn0n1uh3v3ai0t:kiu4qzodo6bwp2m3@klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/kpwjo6ti6ysm389o
 
-
-// let etes = {
-//   pool: true,
-//   connectionLimit: 5,
-//   waitForConnections: true,
-//   module: "sails-mysql",
-//   host: process.env.DATABASE_HOST || "127.0.0.1",
-//   user: process.env.DATABASE_USER || "root",
-//   password: process.env.DATABASE_PASSWORD || "",
-//   database: process.env.DATABASE_DATABASE || "some_database",
-// };
-
-
 let createPoolWith = {};
 if (process.env.JAWSDB_MARIA_URL) {
   createPoolWith = {
--- server/src/index.js
+++ server/src/index.js
@@ -1,105 +1,68 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import { serverPORT } from './config.js';
-import pool from './data/pool.js';
-import bing from './services/bing.js';
-import usersController from './controllers/users-controller.js';
-import authController from './controllers/auth-controller.js';
-import jwtStrategy from './auth/strategy.js';
-import passport from 'passport';
-import adminsController from './controllers/admins-controller.js';
+import { serverPORT } from "./config.js";
+import usersController from "./controllers/users-controller.js";
+import authController from "./controllers/auth-controller.js";
+import jwtStrategy from "./auth/strategy.js";
+import passport from "passport";
+import adminsController from "./controllers/admins-controller.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 const PORT = process.env.PORT || serverPORT;
-import path from 'path';
-import cluster from 'cluster';
-import os from 'os';
-const numCPUs = os.cpus().length;
-const isDev = process.env.NODE_ENV !== 'production';
-
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
+const isDev = process.env.NODE_ENV !== "production";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-// D:\my-websites\ttodorov\server\src
 
 // Multi-process to utilize all CPU cores.
-// if (!isDev && cluster.isMaster) {
-//   console.error(`Node cluster master ${process.pid} is running`);
+import cluster from "cluster";
+import os from "os";
+const numCPUs = os.cpus().length;
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
 
-//   // Fork workers.
-//   for (let i = 0; i < numCPUs; i++) {
-//     cluster.fork();
-//   }
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-//   cluster.on("exit", (worker, code, signal) => {
-//     console.error(
-//       `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
-//     );
-//   });
-// } else {
-//   const app = express();
-//   app.use(cors());
-//   app.use(helmet());
-//   app.use(express.json());
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
+  });
+} else {
+  const app = express();
+  app.use(cors());
+  app.use(helmet());
+  app.use(express.json());
 
-//   // Priority serve any static files.
-//   app.use(express.static(path.resolve(__dirname, "../../client/build")));
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, "../../client/build")));
 
-//   app.use("/users", usersController);
-//   app.use("/admins", adminsController);
+  app.use("/users", usersController);
+  app.use("/admins", adminsController);
 
-//   app.use("/app", express.static("public/avatars"));
+  app.use("/app", express.static("public/avatars"));
 
-//   passport.use(jwtStrategy);
-//   app.use(passport.initialize());
-//   app.use("/auth", authController);
+  passport.use(jwtStrategy);
+  app.use(passport.initialize());
+  app.use("/auth", authController);
 
-//   // All remaining requests return the React app, so it can handle routing.
-//   app.get("*", function (request, response) {
-//     response.sendFile(
-//       path.resolve(__dirname, "../../client/build", "index.html")
-//     );
-//   });
+  // All remaining requests return the React app, so it can handle routing.
+  app.get("*", function (request, response) {
+    response.sendFile(
+      path.resolve(__dirname, "../../client/build", "index.html")
+    );
+  });
 
-//   app.listen(PORT, function () {
-//     console.error(
-//       `Node ${
-//         isDev ? "dev server" : "cluster worker " + process.pid
-//       }: listening on port ${PORT}`
-//     );
-//   });
-// }
-
-
-const app = express();
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
-
-// Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, "../../client/build")));
-
-app.use("/users", usersController);
-app.use("/admins", adminsController);
-
-app.use("/app", express.static("public/avatars"));
-
-passport.use(jwtStrategy);
-app.use(passport.initialize());
-app.use("/auth", authController);
-
-// All remaining requests return the React app, so it can handle routing.
-app.get("*", function (request, response) {
-  response.sendFile(
-    path.resolve(__dirname, "../../client/build", "index.html")
-  );
-});
-
-app.listen(PORT, function () {
-  console.error(
-    `Node ${
-      isDev ? "dev server" : "cluster worker " + process.pid
-    }: listening on port ${PORT}`
-  );
-});
+  app.listen(PORT, function () {
+    console.error(
+      `Node ${
+        isDev ? "dev server" : "cluster worker " + process.pid
+      }: listening on port ${PORT}`
+    );
+  });
+}
