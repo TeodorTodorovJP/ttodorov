b4d5291d174e7eb846eb812427b9a914b857c3f4
Teodor Todorov <ttodorov.jp@gmail.com>
2022-05-14 11:14:38 +0300

    Added CSP meta and Server helmet csp config values

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,22 @@ import { store } from './app/store';
 import App from './App';
 import './index.css';
 
+// document.head.querySelector('meta[http-equiv="Content-Security-Policy"]').setAttribute("content", '');`
+
+// const PORT = process.env.PORT || 'http://127.0.0.1:5000';
+
+// let CSPcontent = `default-src 'self' ; script-src 'self' 'unsafe-inline'; connect-src 'self' ${PORT}; img-src 'self'; style-src 'self' 'unsafe-inline';base-uri 'self';form-action 'self'`
+
+
+// const metaTag: HTMLElement | null = document.head.querySelector("meta[http-equiv='Content-Security-Policy']");
+
+// if (metaTag instanceof HTMLMetaElement) {
+//   metaTag.setAttribute("content", CSPcontent);
+// }
+
+
+// let CSPMeta = metaTags.filter( (meta: Object) => meta["httpEquiv"] === "Content-Security-Policy")
+
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
